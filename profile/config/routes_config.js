86673a238b0231c79b277d04626b0cd02b021c63
cocoasong <cocoasong10@gamil.com>
2019-07-30 19:57:28 +0900

    profile: profile page added

--- profile/config/routes_config.js
+++ profile/config/routes_config.js
@@ -3,6 +3,8 @@ module.exports = {
         {file: './index', path: '/index', method: 'index', type: 'get', passport: false},
         {file: './user', path: '/login', method: 'login', type: 'get' , passport: false},
         {file: './user', path: '/signup', method: 'signup', type: 'get' , passport: false},
+        {file: './user', path: '/profile', method: 'profile', type: 'get' , passport: false},
+        {file: './user', path: '/logout', method: 'logout', type: 'get' , passport: false},
         {file: './passport', path: '/login', method: 'login', type: 'post', passport: true},
         {file: './passport', path: '/signup', method: 'signup', type: 'post', passport: true},
     ]
--- profile/routes/user.js
+++ profile/routes/user.js
@@ -8,5 +8,36 @@ var signup = function(req, res) {
     res.render('signup', {message: req.flash('signupMessage')});
 }
 
+var profile = function(req, res) {
+    console.log('start routing /profile');
+    if(!req.user) {
+        res.redirect('/');
+    }
+    else {
+        var user;
+        if(Array.isArray(req.user)) {
+            user = {
+                name: req.user[0].name,
+                email: req.user[0].email
+            }
+        }
+        else {
+            user = {
+                name: user.name,
+                email: user.email
+            }
+        }
+        res.render('profile', {user: user});
+    }
+};
+
+var logout = function(req, res) {
+    console.log('start routing /logout');
+    req.logout();
+    res.redirect('/')
+};
+
 module.exports.login = login;
+module.exports.logout = logout;
 module.exports.signup = signup;
+module.exports.profile = profile;
